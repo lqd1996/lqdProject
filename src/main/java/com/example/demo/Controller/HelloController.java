f7dae4cc195a8c538d3bcd7f2243d22237e820be
lqd1996 <1786259998@qq.com>
2020-06-27 10:01:25 +0800

    初次提交springboot项目，供学习使用

---
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo.Controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	// 测试接口
+	@RequestMapping(value = "hello")
+	@ResponseBody
+	public String getHello() {
+		String arg = new String();
+		String string = new String();
+		Map<String, Object> map2 = new HashMap<String, Object>();
+		String arg2 = new String();
+		String string2 = new String();
+		return "HelloWord";
+
+	}
+}
